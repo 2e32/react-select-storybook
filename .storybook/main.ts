04905f1905b55dc0476f6ff99c0eae7f77b984b6
konstantin b. <bogucharov.dev@gmail.com>
2024-02-08 15:07:22 +0300

    Add storybook configs

---
+++ .storybook/main.ts
@@ -0,0 +1,24 @@
+import { mergeConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
+  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  viteFinal(config) {
+    // Добавление плагина обработки svg как ReactComponent
+    return mergeConfig(config, { plugins: [svgr()] });
+  },
+};
+export default config;
